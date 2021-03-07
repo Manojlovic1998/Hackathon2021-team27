c2b04cc5b4cb96075dad0fdad38ccbfd36caa7bc
Vlad-404 <vmijat21@gmail.com>
2021-03-08 00:58:05 +0100

    Add anti-cheat: if points are>9 and if NaN to postgame.js

--- static/js/postgame.js
+++ static/js/postgame.js
@@ -1,9 +1,26 @@
 // Displays the total points
 const finalScore = document.createElement('h2');
 document.body.appendChild(finalScore);
-finalScore.innerHTML = `
-    <h2>Your Final Score: ${localStorage.getItem('shamrocks')}<h2>
-`;
+
+// Checks if user has more points than there are questions or manipulated
+// the points through code
+if (localStorage.getItem('shamrocks') > 9) {
+    localStorage.setItem('shamrocks', 'Cheater!');
+    finalScore.innerHTML = `
+        <h2>Cheating is not nice! Rank: Cheater!<h2>
+        `;
+} else {
+    if (localStorage.getItem('shamrocks') == "NaN") {
+        localStorage.setItem('shamrocks', 'Cheater!');
+        finalScore.innerHTML = `
+        <h2>Your Final Score: ${localStorage.getItem('shamrocks')}<h2>
+        `;
+    } else {
+        finalScore.innerHTML = `
+        <h2>Your Final Score: ${localStorage.getItem('shamrocks')}<h2>
+    `;
+    }
+}
 
 // Resets the points
 const backHome = document.getElementById('back-home');
--- static/js/questions.js
+++ static/js/questions.js
@@ -13,3 +13,28 @@ addShamrocks = () => {
 }
 
 correctNext.addEventListener('click', addShamrocks);
+
+// Resets the points if user tries to go back or forward
+// code by tobytailor/history_navigation.js: https://gist.github.com/tobytailor/1164818
+if(window.history && history.pushState){ // check for history api support
+	window.addEventListener('load', function(){
+		// create history states
+		history.pushState(-1, null); // back state
+		history.pushState(0, null); // main state
+		history.pushState(1, null); // forward state
+		history.go(-1); // start in main state
+				
+		this.addEventListener('popstate', function(event, state){
+			// check history state and fire custom events
+			if(state = event.state){
+	
+				event = document.createEvent('Event');
+				event.initEvent(state > 0 ? 'next' : 'previous', true, true);
+				this.dispatchEvent(event);
+				
+				// reset state
+				history.go(-state);
+			}
+		}, false);
+	}, false);
+}
